d72d04aa40e177f235e3f6576527b2c0f3951b06
liviur <livius16@gmail.com>
2014-05-24 21:40:12 +0300

    initial implementation of a search mechanism. Logics will be improved and generalized

--- LRiTunesAppleSearchClient Classes/LRiTunesAppleSearchClient.m
+++ LRiTunesAppleSearchClient Classes/LRiTunesAppleSearchClient.m
@@ -7,7 +7,32 @@
 //
 
 #import "LRiTunesAppleSearchClient.h"
+#import <AFNetworking/AFNetworking.h>
+#import "LRAppleSearchResultItem.h"
+
+NSString * const kAppleSearchAPIURLString = @"https://itunes.apple.com/search?";
 
 @implementation LRiTunesAppleSearchClient
+
+- (void)searchWithTerm:(NSString *)term withCompletionHandler:(void (^)(NSArray *results))completion
+{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    [manager GET:[NSString stringWithFormat:@"%@term=%@",kAppleSearchAPIURLString , [term stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]
+      parameters:nil
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             NSMutableArray *results = [[NSMutableArray alloc] init];
+             NSArray *responseArray = [responseObject objectForKey:@"results"];
+             for (NSDictionary *dict in responseArray)
+             {
+                 LRAppleSearchResultItem *item = [[LRAppleSearchResultItem alloc] initWithDictionary:dict];
+                 [results addObject:item];
+             }
+             completion(results);
+             
+         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             completion(nil);
+         }];
+}
 
 @end
