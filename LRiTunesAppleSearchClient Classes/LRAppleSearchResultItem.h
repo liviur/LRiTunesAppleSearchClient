53bd71d7cd48329c7ac8404aade94dfa764b2b80
liviur <livius16@gmail.com>
2014-05-24 11:56:20 +0300

    Initial workable version of search result item

--- LRiTunesAppleSearchClient Classes/LRAppleSearchResultItem.h
+++ LRiTunesAppleSearchClient Classes/LRAppleSearchResultItem.h
@@ -11,13 +11,15 @@
 typedef NS_ENUM(NSUInteger, LRAppleSearchResultWrapperType) {
     LRAppleSearchResultWrapperTypeTrack,
     LRAppleSearchResultWrapperTypeCollection,
-    LRAppleSearchResultWrapperTypeArtist
+    LRAppleSearchResultWrapperTypeArtist,
+    LRAppleSearchResultWrapperTypeUnknown
 };
 
 typedef NS_ENUM(NSUInteger, LRAppleSearchResultExplicitness) {
     LRAppleSearchResultExplicitnessExplicit,
     LRAppleSearchResultExplicitnessCleanedUp,
     LRAppleSearchResultExplicitnessNonExplicit,
+    LRAppleSearchResultExplicitnessUnknown
 };
 
 typedef NS_ENUM(NSUInteger, LRAppleSearchResultKind) {
@@ -33,15 +35,88 @@ typedef NS_ENUM(NSUInteger, LRAppleSearchResultKind) {
     LRAppleSearchResultKindSoftwarePackage,
     LRAppleSearchResultKindSong,
     LRAppleSearchResultKindTvEpisode,
-    LRAppleSearchResultKindArtist
+    LRAppleSearchResultKindArtist,
+    LRAppleSearchResultKindUnknown
 };
 
 @interface LRAppleSearchResultItem : NSObject
 @property (nonatomic, strong) NSDictionary *modelDictionary;
 
+/**
+ The name of the object returned by the search request.
+ Possible values: track, collection, artist
+ */
 @property (nonatomic, readonly, assign) LRAppleSearchResultWrapperType wrapperType;
+
+/**
+ The Recording Industry Association of America (RIAA) parental advisory for the content returned by the search request.
+ 
+ For more information, see http://itunes.apple.com/WebObjects/MZStore.woa/wa/parentalAdvisory.
+ Possible values: explicit (explicit lyrics, possibly explicit album cover), cleaned (explicit lyrics "bleeped out"), notExplicit (no explicit lyrics)
+ */
 @property (nonatomic, readonly, assign) LRAppleSearchResultExplicitness explicitness;
+
+/**
+ The kind of content returned by the search request.
+ Possible values: book, album, coached-audio, feature-movie, interactive- booklet, music-video, pdf podcast, podcast-episode, software-package, song, tv- episode, artist
+ */
 @property (nonatomic, readonly, assign) LRAppleSearchResultKind kind;
 
-- (void)initWithDictionary:(NSDictionary *)dictionary;
+/**
+ The name of the track, song, video, TV episode, and so on returned by the search request.
+ */
+@property (nonatomic, strong, readonly) NSString *trackName;
+
+/**
+ The name of the artist returned by the search request.
+ */
+@property (nonatomic, strong, readonly) NSString *artistName;
+
+/**
+ The name of the album, TV season, audiobook, and so on returned by the search request.
+ */
+@property (nonatomic, strong, readonly) NSString *collectionName;
+
+/**
+ The name of the album, TV season, audiobook, and so on returned by the search request, with objectionable words *'d out.
+ 
+ Note: Artist names are never censored.
+ */
+@property (nonatomic, strong, readonly) NSString *censoredName;
+
+/**
+ A URL for the artwork associated with the returned media type, sized to 100x100 pixels.
+ Only returned when artwork is available.
+ */
+@property (nonatomic, strong, readonly) NSURL *artworkUrl100;
+
+/**
+ A URL for the artwork associated with the returned media type, sized to 60x60 pixels.
+ Only returned when artwork is available.
+ */
+@property (nonatomic, strong, readonly) NSURL *artworkUrl60;
+
+/**
+ A URL for the content associated with the returned media type. You can click the URL to view the content in the iTunes Store.
+ */
+@property (nonatomic, strong, readonly) NSURL *viewURL;
+
+/**
+ A URL referencing the 30-second preview file for the content associated with the returned media type.
+ Only returned when media type is track.
+ */
+@property (nonatomic, strong, readonly) NSURL *previewUrl;
+
+/**
+ The returned track's time in milliseconds.
+ Only returned when media type is track.
+ */
+@property (nonatomic, readonly, assign) NSInteger trackTimeMillis;
+
+/**
+ Init function that sets the model dictionary
+ @param dictionary - set as the self.ModelDictionary
+ @return Apple Search Result Item
+ */
+- (id)initWithDictionary:(NSDictionary *)dictionary;
 @end
--- LRiTunesAppleSearchClient Classes/LRAppleSearchResultItem.m
+++ LRiTunesAppleSearchClient Classes/LRAppleSearchResultItem.m
@@ -8,6 +8,139 @@
 
 #import "LRAppleSearchResultItem.h"
 
+NSString * const kLRAppleSearchResultWrapperTypeKey = @"wrapperType";
+NSString * const kLRAppleSearchResultExplicitnessKey = @"explicitness";
+NSString * const kLRAppleSearchResultKindKey = @"kind";
+NSString * const kLRAppleSearchResultTrackNameKey = @"trackName";
+NSString * const kLRAppleSearchResultArtistNameKey = @"artistName";
+NSString * const kLRAppleSearchResultCollectionNameKey = @"collectionName";
+NSString * const kLRAppleSearchResultCensodredNameKey = @"censoredName";
+NSString * const kLRAppleSearchResultArtworkUrl100Key = @"artworkUrl100";
+NSString * const kLRAppleSearchResultArtworkUrl60Key = @"artworkUrl60";
+NSString * const kLRAppleSearchResultViewUrlKey = @"viewURL";
+NSString * const kLRAppleSearchResultPreviewUrlKey = @"previewUrl";
+NSString * const kLRAppleSearchResultTrackTimeMillisKey = @"trackTimeMillis";
+
 @implementation LRAppleSearchResultItem
+
+- (LRAppleSearchResultWrapperType)wrapperType
+{
+    LRAppleSearchResultWrapperType returnValue = LRAppleSearchResultWrapperTypeUnknown;
+    NSString *wrapperTypeString = [self.modelDictionary objectForKey:kLRAppleSearchResultWrapperTypeKey];
+    if ([wrapperTypeString isEqualToString:@"track"]) {
+        returnValue = LRAppleSearchResultWrapperTypeTrack;
+    } else if ([wrapperTypeString isEqualToString:@"collection"]) {
+        returnValue = LRAppleSearchResultWrapperTypeCollection;
+    } else if ([wrapperTypeString isEqualToString:@"artist"]) {
+        returnValue = LRAppleSearchResultWrapperTypeArtist;
+    }
+
+    return returnValue;
+}
+
+- (LRAppleSearchResultExplicitness)explicitness
+{
+    LRAppleSearchResultExplicitness returnValue = LRAppleSearchResultExplicitnessUnknown;
+    NSString *explicitnessString = [self.modelDictionary objectForKey:kLRAppleSearchResultExplicitnessKey];
+    if ([explicitnessString isEqualToString:@"explicit"]) {
+        returnValue = LRAppleSearchResultExplicitnessExplicit;
+    } else if ([explicitnessString isEqualToString:@"cleanedUp"]) {
+        returnValue = LRAppleSearchResultExplicitnessCleanedUp;
+    } else if ([explicitnessString isEqualToString:@"nonExplicit"]) {
+        returnValue = LRAppleSearchResultExplicitnessNonExplicit;
+    }
+    
+    return returnValue;
+}
+
+- (LRAppleSearchResultKind)kind
+{
+    LRAppleSearchResultKind returnValue = LRAppleSearchResultKindUnknown;
+    NSString *kindString = [self.modelDictionary objectForKey:kLRAppleSearchResultKindKey];
+    if ([kindString isEqualToString:@"book"]) {
+        returnValue = LRAppleSearchResultKindBook;
+    } else if ([kindString isEqualToString:@"album"]) {
+        returnValue = LRAppleSearchResultKindAlbum;
+    } else if ([kindString isEqualToString:@"Coached-audio"]) {
+        returnValue = LRAppleSearchResultKindCoachedAudio;
+    } else if ([kindString isEqualToString:@"feature-movie"]) {
+        returnValue = LRAppleSearchResultKindFeatureMovie;
+    } else if ([kindString isEqualToString:@"interactive-booklet"]) {
+        returnValue = LRAppleSearchResultKindInteractiveBooklet;
+    } else if ([kindString isEqualToString:@"musicVideo"]) {
+        returnValue = LRAppleSearchResultKindMusicVideo;
+    } else if ([kindString isEqualToString:@"pdf"]) {
+        returnValue = LRAppleSearchResultKindPdf;
+    } else if ([kindString isEqualToString:@"podcast"]) {
+        returnValue = LRAppleSearchResultKindPodcast;
+    } else if ([kindString isEqualToString:@"podcast-episode"]) {
+        returnValue = LRAppleSearchResultKindPodcastEpisode;
+    } else if ([kindString isEqualToString:@"software-package"]) {
+        returnValue = LRAppleSearchResultKindSoftwarePackage;
+    } else if ([kindString isEqualToString:@"song"]) {
+        returnValue = LRAppleSearchResultKindSong;
+    } else if ([kindString isEqualToString:@"tv-episode"]) {
+        returnValue = LRAppleSearchResultKindTvEpisode;
+    } else if ([kindString isEqualToString:@"artist"]) {
+        returnValue = LRAppleSearchResultKindArtist;
+    }
+    
+    return returnValue;
+}
+
+- (NSString *)trackName
+{
+    return [self.modelDictionary objectForKey:kLRAppleSearchResultTrackNameKey];
+}
+
+- (NSString *)artistName
+{
+    return [self.modelDictionary objectForKey:kLRAppleSearchResultArtistNameKey];
+}
+
+- (NSString *)collectionName
+{
+    return [self.modelDictionary objectForKey:kLRAppleSearchResultCollectionNameKey];
+}
+
+- (NSString *)censoredName
+{
+    return [self.modelDictionary objectForKey:kLRAppleSearchResultCensodredNameKey];
+}
+
+- (NSURL *)artworkUrl100
+{
+    return [NSURL URLWithString:[self.modelDictionary objectForKey:kLRAppleSearchResultArtworkUrl100Key]];
+}
+
+- (NSURL *)artworkUrl60
+{
+    return [NSURL URLWithString:[self.modelDictionary objectForKey:kLRAppleSearchResultArtworkUrl60Key]];
+}
+
+- (NSURL *)viewURL
+{
+    return [NSURL URLWithString:[self.modelDictionary objectForKey:kLRAppleSearchResultViewUrlKey]];
+}
+
+- (NSURL *)previewUrl
+{
+    return [NSURL URLWithString:[self.modelDictionary objectForKey:kLRAppleSearchResultPreviewUrlKey]];
+}
+
+- (NSInteger)trackTimeMillis
+{
+    return [[self.modelDictionary objectForKey:kLRAppleSearchResultTrackTimeMillisKey] integerValue];
+}
+
+- (id)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [self init];
+    if (self) {
+        self.modelDictionary = dictionary;
+    }
+    
+    return self;
+}
 
 @end
